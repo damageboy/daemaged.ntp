858aab07d2396c223e2befc7e8a988a2dc7b0172
Shay Rojansky <roji@eaglets.co.il>
2013-06-05 16:05:54 +0100

    Some work on the NtpTimeKeeper
    Some all-around cleanup/refactoring

--- NtpTimeKeeper.cs
+++ NtpTimeKeeper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading;
 
 namespace Daemaged.NTP
@@ -8,49 +9,77 @@ namespace Daemaged.NTP
   /// </summary>
   public static class NtpTimeKeeper
   {
+    #region Private state
+
     private static int _sleepPeriod;
-    private static Thread _keeperThread;   
+    private static Thread _syncThread;
     private static long _timeDiff;
-    private static int _stratum;    
+    private static int _stratum;
     private static DateTime _lastTransmit;
     private static Ntp _ntp;
     private static readonly ManualResetEvent _stopEvent;
 
+    #endregion
+
+    #region Configuration settings
+
+    /// <summary>
+    /// The NTP host to connect to. Defaults to pool.ntp.org.
+    /// </summary>
+    public static string NtpHost { get; set; }
+
+    /// <summary>
+    /// The number of seconds to wait between NTP synchronizations. Defaults to 60.
+    /// </summary>
+    public static int PollingTime { get; set; }
+
+    /// <summary>
+    /// The number of milliseconds to wait for an NTP response before an exception will be thrown. Defaults to 3000.
+    /// </summary>
+    public static int Timeout { get; set; }
+
+    #endregion
+
+    /// <summary>
+    /// An event that will be fired every time a sync fails.
+    /// </summary>
+    public static EventHandler<ErrorEventArgs> FailedSync;
+
+    /// <summary>
+    /// An event that will be fired every time a sync succeeds.
+    /// </summary>
+    public static EventHandler Synced { get; set; }
 
     static NtpTimeKeeper()
     {
       _stopEvent = new ManualResetEvent(false);
+      NtpHost = "pool.ntp.org";
+      Timeout = 3000;
+      PollingTime = 60;
     }
 
     /// <summary>
     /// Start collecting NTP time differences
     /// </summary>
-    /// <param name="ntpHost">the hostname for the NTP server</param>
-    /// <param name="everySeconds">update the time offset every N seconds</param>
-    public static bool Start(string ntpHost, int everySeconds = 60)
+    public static void StartAutoSync()
     {
-      Stop();
-      _sleepPeriod = everySeconds * 1000;      
+      if (_syncThread != null)
+        throw new InvalidOperationException("Timekeeper is already started");
 
-      _ntp = new Ntp(ntpHost) { VersionNumber = 4 };
+      _ntp = new Ntp(NtpHost) {VersionNumber = 4, Timeout = Timeout};
 
-      // Do it once to kick-start everything
-      try {
-        Keep();
-      }
-      catch (Exception e) {
-        return false;
-      }
+      Sync();
 
+      _sleepPeriod = PollingTime*1000;
       _stopEvent.Reset();
-      
+
       // Assuming this worked at least once, lets do it again...
-      _keeperThread = new Thread(KeepNtpTime) {
-        IsBackground = true,
-        Name = "NtpTimeKeeper"
-      };
-      _keeperThread.Start();
-      return true;
+      _syncThread = new Thread(KeepNtpTime)
+                      {
+                        IsBackground = true,
+                        Name = "NtpTimeKeeper"
+                      };
+      _syncThread.Start();
     }
 
     /// <summary>
@@ -58,28 +87,38 @@ namespace Daemaged.NTP
     /// </summary>
     public static void Stop()
     {
-      _stopEvent.Set();
-      if (_keeperThread == null)
+      if (!IsSyncing)
         return;
-      _keeperThread.Join();
-      _keeperThread = null;
+      _stopEvent.Set();
+      _syncThread.Join();
+      _syncThread = null;
+    }
+
+    public static bool IsSyncing
+    {
+      get { return _syncThread != null; }
     }
 
     private static void KeepNtpTime()
     {
-      // Keep waiting until the even is actually signal, then just GTFO
-      while (!_stopEvent.WaitOne(_sleepPeriod)) {
-        try {
-          Keep();
+      // Loop forever, syncing every _sleep period, until the stop event is set
+      while (!_stopEvent.WaitOne(_sleepPeriod))
+      {
+        try
+        {
+          Sync();
+          if (Synced != null)
+            Synced(null, null);
         }
-        catch {
-          if (NtpError != null)
-            NtpError(null, null);
+        catch (Exception e)
+        {
+          if (FailedSync != null)
+            FailedSync(null, new ErrorEventArgs(e));
         }
       }
     }
 
-    private static void Keep() 
+    private static void Sync()
     {
       var resp = _ntp.GetTime();
       _timeDiff = resp.TimeOffset.ToTicks();
@@ -87,27 +126,53 @@ namespace Daemaged.NTP
       _stratum = resp.Stratum;
     }
 
-    public static EventHandler NtpError;
+    #region Time accessors
 
     /// <summary>
     /// UtcNow  adjusted to the latest recovered time offset in ticks
     /// </summary>
-    public static long AdjustedUtcNowTicks { get { return DateTime.UtcNow.Ticks + _timeDiff; } }
+    public static long AdjustedUtcNowTicks
+    {
+      get { return DateTime.UtcNow.Ticks + _timeDiff; }
+    }
+
     /// <summary>
     /// UtcNow adjusted to the latest recovered time offset
     /// </summary>
-    public static DateTime AdjustedUtcNow { get { return DateTime.UtcNow.AddTicks(_timeDiff); } }
+    public static DateTime AdjustedUtcNow
+    {
+      get { return DateTime.UtcNow.AddTicks(_timeDiff); }
+    }
+
     /// <summary>
     /// The latest recovered time offset in ticks
     /// </summary>
-    public static long LastRecordedDiffTime { get { return _timeDiff; } }
+    public static long LastRecordedDiffTime
+    {
+      get { return _timeDiff; }
+    }
+
     /// <summary>
     /// The NTP server's Stratum value
     /// </summary>
-    public static int Stratum { get { return _stratum; } } 
+    public static int Stratum
+    {
+      get { return _stratum; }
+    }
+
     /// <summary>
     /// The time-stamp when the last successful transition from the server was made
     /// </summary>
-    public static DateTime LastTransmitTime { get { return _lastTransmit; } }
+    public static DateTime LastTransmitTime
+    {
+      get { return _lastTransmit; }
+    }
+
+    public static TimeSpan TimeSinceLastSuccessfulSync
+    {
+      get { return DateTime.UtcNow - LastTransmitTime + TimeSpan.FromTicks(_timeDiff); }
+    }
+
+    #endregion
   }
 }
